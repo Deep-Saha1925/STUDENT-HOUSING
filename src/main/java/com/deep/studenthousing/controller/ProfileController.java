a41d8b280eb5a74e9de982b3ae73536346761819
Deep Saha <dip23447@gmail.com>
2025-09-11 12:13:25 +0530

    Added the profile controller class which shows details of a user.

---
+++ src/main/java/com/deep/studenthousing/controller/ProfileController.java
@@ -0,0 +1,31 @@
+package com.deep.studenthousing.controller;
+
+import com.deep.studenthousing.entity.User;
+import com.deep.studenthousing.service.UserService;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class ProfileController {
+
+    private final UserService userService;
+
+    public ProfileController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/profile/{id}")
+    public String viewProfile(@PathVariable Long id, Model model){
+        User user = userService.findById(id);
+
+        if(user != null){
+            model.addAttribute("user", user);
+            return "profile";
+        }else{
+            throw new RuntimeException("user not found!!");
+        }
+    }
+}
