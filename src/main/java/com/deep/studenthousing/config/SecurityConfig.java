080c66d36ecb4fe4c266a55bac2b26866e57f9ea
Deep Saha <dip23447@gmail.com>
2025-09-13 12:23:50 +0530

    modifed admin dashboard ad login page with error controller.

--- src/main/java/com/deep/studenthousing/config/SecurityConfig.java
+++ src/main/java/com/deep/studenthousing/config/SecurityConfig.java
@@ -19,7 +19,8 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/","/users/register", "/css/**", "/js/**").permitAll()
+                        .requestMatchers("/users/register").hasAnyRole("ADMIN")
+                        .requestMatchers("/", "/css/**", "/js/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(form -> form
--- src/main/java/com/deep/studenthousing/controller/UserController.java
+++ src/main/java/com/deep/studenthousing/controller/UserController.java
@@ -4,6 +4,7 @@ import com.deep.studenthousing.entity.Role;
 import com.deep.studenthousing.entity.User;
 import com.deep.studenthousing.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -24,19 +25,30 @@ public class UserController {
         this.userService = userService;
     }
 
+    // registration form
     @GetMapping("/register")
-    public String registerForm(Model model) {
-        model.addAttribute("user", new User());
-        return "register";
+    public String registerForm(Model model, Authentication authentication) {
+
+        if(authentication != null && authentication.getAuthorities().stream()
+                .anyMatch(a -> a.getAuthority().equals("ROLE_ADMIN"))){
+
+            model.addAttribute("user", new User());
+            return "register";
+        }
+        // redirect non-admins
+        return "redirect:/access-denied";
     }
 
     @PostMapping("/register")
-    public String register(@ModelAttribute("user") User user) {
-        if (user.getRole() == null) {
-            user.setRole(Role.STUDENT); // default role
+    public String register(@ModelAttribute("user") User user, Authentication authentication) {
+
+        if(authentication != null && authentication.getAuthorities().stream()
+                .anyMatch(a -> a.getAuthority().equals("ROLE_ADMIN"))){
+
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
+            userService.save(user);
+            return "redirect:/admin/dashboard?success";
         }
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        userService.save(user);
-        return "redirect:/login";
+        return "redirect:/access-denied";
     }
 }
