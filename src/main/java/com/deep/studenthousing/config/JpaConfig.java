8376798b8540e05eb723254745ec6687c6144f8c
Deep-Saha1925 <dip23447@gmail.com>
2026-05-15 14:26:36 +0530

    JPA configration.

---
+++ src/main/java/com/deep/studenthousing/config/JpaConfig.java
@@ -0,0 +1,12 @@
+package com.deep.studenthousing.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+public class JpaConfig {
+
+}
