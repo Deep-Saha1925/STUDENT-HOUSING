ae6bf67530e7c28d373fdf17613430da7a6de3d4
Deep-Saha1925 <dip23447@gmail.com>
2026-05-16 20:37:46 +0530

    Added config for local storage bean.

--- src/main/java/com/deep/studenthousing/config/JpaConfig.java
+++ src/main/java/com/deep/studenthousing/config/JpaConfig.java
@@ -1,12 +1,41 @@
 package com.deep.studenthousing.config;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
 
 @Configuration
 @EnableTransactionManagement
 public class JpaConfig {
+
+//    LOCAL EntityManager
+    @Bean
+    @Primary
+    public LocalContainerEntityManagerFactoryBean localEntityManagerFactory(
+            @Qualifier("localDataSource")DataSource dataSource
+            ){
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(dataSource);
+        em.setPackagesToScan("com.deep.studenthousing.entity");
+
+        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+        em.setJpaVendorAdapter(adapter);
+
+        Properties properties = new Properties();
+        properties.put("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+        properties.put("hibernate.hbm2ddl.auto", "update");
+        properties.put("hibernate.show_sql", "true");
+        em.setJpaProperties(properties);
+
+        return em;
+    }
+
 
 }
