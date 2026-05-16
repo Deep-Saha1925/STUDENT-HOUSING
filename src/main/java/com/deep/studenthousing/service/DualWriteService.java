707b521b3272a3f0551bde0c5331fd2a0fe62d88
Deep-Saha1925 <dip23447@gmail.com>
2026-05-16 21:35:01 +0530

    Save to local and cloud db.

--- src/main/java/com/deep/studenthousing/service/DualWriteService.java
+++ src/main/java/com/deep/studenthousing/service/DualWriteService.java
@@ -3,6 +3,7 @@ package com.deep.studenthousing.service;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class DualWriteService {
@@ -13,6 +14,19 @@ public class DualWriteService {
     @PersistenceContext(unitName = "cloudEntityManagerFactory")
     private EntityManager cloudEm;
 
+    @Transactional("localTransactionManager")
+    public <T> void saveLocal(T entity){
+        localEm.merge(entity);
+    }
 
+    @Transactional("cloudTransactionManager")
+    public <T> void saveCloud(T entity){
+        cloudEm.merge(entity);
+    }
+
+    public <T> void saveBoth(T entity){
+        saveLocal(entity);
+        saveCloud(entity);
+    }
 
 }
