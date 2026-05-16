670a88c735b51dd5afa8fa13e078079db61ced46
Deep-Saha1925 <dip23447@gmail.com>
2026-05-16 20:46:41 +0530

    Local Transaction manager.

--- src/main/java/com/deep/studenthousing/config/JpaConfig.java
+++ src/main/java/com/deep/studenthousing/config/JpaConfig.java
@@ -1,11 +1,14 @@
 package com.deep.studenthousing.config;
 
+import jakarta.persistence.EntityManagerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
@@ -58,5 +61,13 @@ public class JpaConfig {
         return em;
     }
 
+//    Transaction Managers
+    @Bean(name = "localTransactionManager")
+    @Primary
+    public PlatformTransactionManager localTransactionManager(
+            @Qualifier("localEntityManagerFactory")EntityManagerFactory emf
+    ){
+        return new JpaTransactionManager(emf);
+    }
 
 }
