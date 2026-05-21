7f9ac52e041f25923f004fbe92bba2fcde6329cf
Deep-Saha1925 <dip23447@gmail.com>
2026-05-21 14:16:41 +0530

    Extracted lat and lon.

--- src/main/java/com/deep/studenthousing/service/GeoCodingService.java
+++ src/main/java/com/deep/studenthousing/service/GeoCodingService.java
@@ -1,8 +1,10 @@
 package com.deep.studenthousing.service;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -30,10 +32,20 @@ public class GeoCodingService {
             headers.set("User-Agent", "StudentHousingApp/1.0");
 
             HttpEntity<String> entity = new HttpEntity<>(headers);
-//            ResponseEntity<String> response = new ResponseEntity<>()
+            ResponseEntity<String> response =
+                    restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+
+            JsonNode root = objectMapper.readTree(response.getBody());
+
+            if(root.isArray() && !root.isEmpty()){
+                double lat = root.get(0).get("lat").asDouble();
+                double lon = root.get(1).get("lon").asDouble();
+                return new double[]{lat, lon};
+            }
 
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            System.err.println("Geocoding failed for: " + city + ", " + area);
+            e.printStackTrace();
         }
 
         return null;
