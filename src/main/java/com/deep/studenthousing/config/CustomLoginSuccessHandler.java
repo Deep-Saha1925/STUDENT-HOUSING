44a39c11045c17ff53dd24776b44c522f5014f56
Deep-Saha1925 <dip23447@gmail.com>
2026-07-05 22:39:50 +0530

    Extending SavedRequestAwareAuthenticationSuccessHandler class.

--- src/main/java/com/deep/studenthousing/config/CustomLoginSuccessHandler.java
+++ src/main/java/com/deep/studenthousing/config/CustomLoginSuccessHandler.java
@@ -8,14 +8,18 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 
 import java.io.IOException;
 
 @Configuration
-public class CustomLoginSuccessHandler implements AuthenticationSuccessHandler {
+public class CustomLoginSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
 
     private final UserRepository userRepo;
+    private final RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
 
     public CustomLoginSuccessHandler(UserRepository userRepo) {
         this.userRepo = userRepo;
@@ -24,26 +28,17 @@ public class CustomLoginSuccessHandler implements AuthenticationSuccessHandler {
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
 
-        System.out.println("SUCCESS HANDLER CALLED");
-        System.out.println(authentication.getAuthorities());
-        System.out.println(authentication.getName());
-
         String email = authentication.getName();
         User user = userRepo.findByEmail(email);
 
-        switch (user.getRole()) {
-            case STUDENT:
-                response.sendRedirect("/student/dashboard");
-                break;
-            case OWNER:
-                response.sendRedirect("/properties/owner/" + user.getId());
-                break;
-            case ADMIN:
-                response.sendRedirect("/admin/dashboard");
-                break;
-            default:
-                response.sendRedirect("/login?error=true");
-                break;
-        }
+        String targetUrl = switch (user.getRole()) {
+            case STUDENT -> "/student/dashboard";
+            case OWNER -> "/properties/owner/" + user.getId();
+            case ADMIN -> "/admin/dashboard";
+            default -> "/login?error=true";
+        };
+
+        clearAuthenticationAttributes(request);
+        redirectStrategy.sendRedirect(request, response, targetUrl);
     }
 }
