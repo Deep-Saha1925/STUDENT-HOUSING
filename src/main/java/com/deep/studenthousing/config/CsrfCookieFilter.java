a2244754546ac76dec9b0711026267db9ee3d0d7
Deep-Saha1925 <dip23447@gmail.com>
2026-07-06 21:55:10 +0530

    Extending Onceperrequest filter chain.

--- src/main/java/com/deep/studenthousing/config/CsrfCookieFilter.java
+++ src/main/java/com/deep/studenthousing/config/CsrfCookieFilter.java
@@ -1,4 +1,16 @@
 package com.deep.studenthousing.config;
 
-public class CsrfCookieFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+public class CsrfCookieFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException {
+
+    }
+
 }
