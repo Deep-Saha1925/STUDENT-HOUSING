41fcc2ea2ff8534ad600254a51400ff656c549df
Deep-Saha1925 <dip23447@gmail.com>
2026-07-17 15:17:10 +0530

    Updated GlobalExceptionHandler class

--- src/main/java/com/deep/studenthousing/exception/GlobalExceptionHandler.java
+++ src/main/java/com/deep/studenthousing/exception/GlobalExceptionHandler.java
@@ -1,8 +1,10 @@
 package com.deep.studenthousing.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -11,5 +13,12 @@ public class GlobalExceptionHandler {
     public String handleUserNotFound(UserNotFoundException ex, Model model){
         model.addAttribute("errorMessage", ex.getMessage());
         return "error-page";
+    }
+
+    @ExceptionHandler(UnauthorizedActionException.class)
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public String handleUnauthorizedAction(UnauthorizedActionException ex, Model model){
+        model.addAttribute("errorMessage", ex.getMessage());
+        return "unauthorized";
     }
 }
