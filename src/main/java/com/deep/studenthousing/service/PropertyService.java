02cbcd6b5a094a7c67097211eaafa9f82235d403
Deep-Saha1925 <dip23447@gmail.com>
2026-09-09 23:58:09 +0530

    Updated PropertyService query for searching

--- src/main/java/com/deep/studenthousing/service/PropertyService.java
+++ src/main/java/com/deep/studenthousing/service/PropertyService.java
@@ -64,8 +64,8 @@ public class PropertyService {
         return propertyRepository.findByCityContainingIgnoreCase(query);
     }
 
-    public List<Property> searchProperties(String city, Double rent, String rentalType) {
-        return propertyRepository.searchProperties(city, rent, rentalType);
+    public List<Property> searchProperties(String city, Double rent, String rentalType, String gender) {
+        return propertyRepository.searchProperties(city, rent, rentalType, gender);
     }
 
 
